dbfd9a52d38322aa3ab83d8f8d9777cdf522e0ce
1862325 <1862325@cstj.qc.ca>
2020-05-21 10:52:29 -0400

    bouton scroll to top

--- js/eventAnimationScript.js
+++ js/eventAnimationScript.js
@@ -38,3 +38,22 @@ $(window).scroll(function(){
         $('.nav-wrapper').removeClass('sticky');
     }
 });
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// Bouton scroll to top
+// - - - - - - - - - - - - - - - - - - - - - - - - - - -
+$(document).ready(function(){
+
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 40){
+            $('#top-btn').fadeIn();
+        }else{
+            $('#top-btn').fadeOut();
+        }
+    });
+
+    $("#top-btn").click(function(){
+        $('html, body').animate({scrollTop : 0}, 200);
+    });
+  });
+          
